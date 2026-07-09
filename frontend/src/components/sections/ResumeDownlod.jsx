fbe113a460298d738d02c5d76206fd82b4a8a826
LalitMohanAgnihotri <lalitagnihotri62@gmail.com>
2026-07-09 22:47:41 +0530

    Update resume PDF

--- frontend/src/components/sections/ResumeDownlod.jsx
+++ frontend/src/components/sections/ResumeDownlod.jsx
@@ -7,7 +7,7 @@ const ResumeDownlod = () => {
   const [isImageEnlarged, setIsImageEnlarged] = useState(false);
 
   const resumeTitle = "Resume Preview";
-  const pdfPath = "/Programming with JavaScript.pdf";
+  const pdfPath = "/Lalit_Mohan_Agnihotri_Resume.pdf";
 
   return (
     <>
